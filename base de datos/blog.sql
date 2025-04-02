e29e57f638a4cb37998df460845301c83b6d0d21
Mario051105 <mballato0@ucol.mx>
2025-04-02 13:04:19 -0600

    Avances

---
+++ base de datos/blog.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-03-2025 a las 22:55:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+CREATE TABLE `entradas` (
+  `id_entrada` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL,
+  `contenido` longtext DEFAULT NULL,
+  `id_imagen` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagenes`
+--
+
+CREATE TABLE `imagenes` (
+  `id_imagen` int(11) NOT NULL,
+  `imagen` varchar(200) DEFAULT NULL,
+  `id_entrada` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `contrasenia` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD PRIMARY KEY (`id_entrada`),
+  ADD KEY `id_imagen` (`id_imagen`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  ADD PRIMARY KEY (`id_imagen`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD CONSTRAINT `entradas_ibfk_1` FOREIGN KEY (`id_imagen`) REFERENCES `imagenes` (`id_imagen`),
+  ADD CONSTRAINT `entradas_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ lang_en.php
@@ -0,0 +1,45 @@
+<?php
+$idioma = [
+  // Crear publicaciones
+  "crear_publicacion_title" => "Create New Post",
+  "titulo_label"            => "Title:",
+  "categoria_label"         => "Category:",
+  "contenido_label"         => "Content:",
+  "imagen_label"            => "Image / Banner:",
+  "cita"                    => "Source: ",
+  "crear_button"            => "Create Post",
+  // Mensajes
+  "publicacion_success"     => "Post created successfully.",
+  "publicacion_error"       => "Error creating post:",
+  "error_subir_imagen"      => "Error uploading the image.",
+  // General
+  "voces_proceso"           => "Voices of the Process",
+  "menu"                    => "MENU",
+  "buscar"                  => "Search...",
+  "cerrar"                  => "Close",
+  "inicio"                  => "Home",
+  "noticias"                => "News",
+  "contacto"                => "Contact",
+  "acerca_de"               => "About",
+  "login"                   => "Login",
+  "footer_text"             => "&copy; 2025 Voices of the Process. All rights reserved.",
+  // Main
+  "main_banner_title"       => "Latest Post Banner",
+  "leer_mas"                => "Read more",
+  // Registro
+  "login_title"             => "Login",
+  "login_subtitle"          => "Welcome back! Log in to your account to record your club's attendance.",
+  "login_placeholder"       => "Username or email",
+  "login_password_placeholder" => "Password",
+  "login_button"            => "Log In",
+  "crear_cuenta"            => "Create a new account",
+  "registro_title"          => "Account Registration",
+  "nombre_placeholder"      => "First Name",
+  "primer_apellido_placeholder" => "Last Name",
+  "segundo_apellido_placeholder" => "Second Last Name",
+  "registro_correo_placeholder"  => "Username or email",
+  "registro_password_placeholder" => "Password",
+  "registrar_button"        => "Register",
+  "iniciar_sesion"          => "Log in"
+];
+?>
