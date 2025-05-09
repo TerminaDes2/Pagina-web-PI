c5825bd54e2946be585351753c9c6cebaeb620c1
DOMINIC ISAI GARCIA BAUTISTA <dgarcia70@ucol.mx>
2025-05-09 16:59:38 -0600

    arreglo de las rutas

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/Pagina-web-PI/assets/css/header-footer.css">
+<link rel="stylesheet" href="../assets/css/header-footer.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet">
@@ -11,14 +11,14 @@
         </button>
         <div class="hf-logo-container">
             <div class="hf-logo">
-                <a href="/Pagina-web-PI/index.php">
+                <a href="/index.php">
                     <h1>Voces del Proceso</h1>
                 </a>
             </div>
         </div>
         <nav class="hf-main-nav">
             <ul class="hf-menu-desplegable">
-                <li><a href="/Pagina-web-PI/index.php"><?= $translator->__("Inicio") ?></a></li>
+                <li><a href="/index.php"><?= $translator->__("Inicio") ?></a></li>
                 <li class="hf-menu-desplegable">
                     <a href="#"><?= $translator->__("Noticias") ?></a>
                     <div class="hf-contenido-desplegable"></div>
