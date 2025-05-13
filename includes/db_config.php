2c0b86e9abe6b916d00c647bdae03259deafefc1
DOMINIC ISAI GARCIA BAUTISTA <dgarcia70@ucol.mx>
2025-05-13 07:38:02 -0600

    Add files via upload
    a ver si así no da erroees

---
+++ includes/db_config.php
@@ -0,0 +1,7 @@
+<?php
+// Configuración de la base de datos
+if (!defined('host')) define('host', 'localhost');
+if (!defined('dbname')) define('dbname', 'blog');
+if (!defined('dbuser')) define('dbuser', 'root');
+if (!defined('dbpass')) define('dbpass', 'administrador');
+?>
